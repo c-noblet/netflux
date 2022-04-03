c085af3222a3e0dedbdc5fe7e1133e7ac78a01a2
c-noblet <c.noblet.dev@gmail.com>
2022-04-03 14:46:37 +0200

    finish detail screen

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -1,7 +1,7 @@
-import 'package:firebase_auth/firebase_auth.dart';
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:flutter_html/flutter_html.dart';
 import 'package:netflux/models/show_model.dart';
-import '../services/theme_builder.dart';
 import 'package:http/http.dart' as http;
 
 class DetailScreen extends StatefulWidget {
@@ -12,16 +12,40 @@ class DetailScreen extends StatefulWidget {
 }
 
 class _DetailScreenState extends State<DetailScreen> {
+  late Future<Show> show;
+
+  Future<Show> fetchShow(id) async {
+    final response = await http.get(Uri.parse('https://api.tvmaze.com/shows/' + id.toString()));
+  
+    if (response.statusCode == 200) {
+      final parsed = json.decode(response.body).cast<String, dynamic>();
+      return Show.fromMap(parsed);
+    } else {
+      throw Exception('Failed to load show');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     final id = ModalRoute.of(context)!.settings.arguments as int;
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(id.toString()),
-      ),
-      body: SafeArea(
-        child: Text('Detail page'),
-      ),
+    show = fetchShow(id);
+
+    return FutureBuilder<Show>(
+      future: show,
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return Scaffold(
+            appBar: AppBar(
+              title: Text(snapshot.data!.name),
+            ),
+            body: SafeArea(
+              child: Html(data: snapshot.data!.summary.toString()),
+            ),
+          );
+        } else {
+          return const Center(child: CircularProgressIndicator());
+        }
+      },
     );
   }
 }
