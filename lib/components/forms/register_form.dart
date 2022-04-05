35e9b5c1b3fb464554c5c88af5e859bacb10717d
vgimonnet <valentin.gimonnet18@gmail.com>
2022-04-05 18:37:40 +0200

    Update form register to store user datas

--- lib/components/forms/register_form.dart
+++ lib/components/forms/register_form.dart
@@ -1,5 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:datetime_picker_formfield/datetime_picker_formfield.dart';
+import 'package:netflux/models/user_model.dart';
 
 class RegisterForm extends StatefulWidget {
   const RegisterForm({Key? key}) : super(key: key);
@@ -10,15 +14,21 @@ class RegisterForm extends StatefulWidget {
 
 class _RegisterFormState extends State<RegisterForm> {
   final key = GlobalKey<FormState>();
-  String email = '', password = '', errorMessage = '';
+  String 
+    email = '', 
+    password = '', 
+    errorMessage = '';
+
+  UserModel user = UserModel(firstname: '', lastname: '', country: '', city: '', birthdate: '', image: 'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.mycustomer.com%2Fsites%2Fall%2Fthemes%2Fpp%2Fimg%2Fdefault-user.png&f=1&nofb=1');
 
   void signInToFirebase(String email, String password) async {
     try {
       await FirebaseAuth.instance
           .createUserWithEmailAndPassword(email: email, password: password)
-          .then(
-            (value) => Navigator.of(context).pushReplacementNamed('/'),
-          );
+          .then((value) async {
+            storeUserDataToFirestore(value.user!.uid);
+            Navigator.of(context).pushReplacementNamed('/');
+          });
 
     } on FirebaseException catch (error) {
       if (error.code == 'weak-password') {
@@ -30,6 +40,21 @@ class _RegisterFormState extends State<RegisterForm> {
           errorMessage = 'email-already-in-use';
         });
       }
+    } catch (error) {
+      setState(() {
+        errorMessage = error.toString();
+      });
+    }
+  }
+
+  void storeUserDataToFirestore(String? uid) async {
+    try {
+      await FirebaseFirestore
+        .instance
+        .collection('users')
+        .doc(uid)
+        .set(user.toMap())
+      ;
     } catch (error) {
       setState(() {
         errorMessage = error.toString();
@@ -84,6 +109,100 @@ class _RegisterFormState extends State<RegisterForm> {
                 });
               },
               decoration: const InputDecoration(hintText: 'Password'),
+            ),
+            const SizedBox(
+              height: 25,
+            ),
+            TextFormField(
+              validator: (String? value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter some text';
+                }
+                return null;
+              },
+              onSaved: (String? value) {
+                setState(() {
+                  user.firstname = value!;
+                });
+              },
+              decoration: const InputDecoration(hintText: 'Firstname'),
+            ),
+            const SizedBox(
+              height: 25,
+            ),
+            TextFormField(
+              validator: (String? value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter some text';
+                }
+                return null;
+              },
+              onSaved: (String? value) {
+                setState(() {
+                  user.lastname = value!;
+                });
+              },
+              decoration: const InputDecoration(hintText: 'Lastname'),
+            ),
+            const SizedBox(
+              height: 25,
+            ),
+            TextFormField(
+              validator: (String? value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter some text';
+                }
+                return null;
+              },
+              onSaved: (String? value) {
+                setState(() {
+                  user.city = value!;
+                });
+              },
+              decoration: const InputDecoration(hintText: 'City'),
+            ),
+            const SizedBox(
+              height: 25,
+            ),
+            TextFormField(
+              validator: (String? value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter some text';
+                }
+                return null;
+              },
+              onSaved: (String? value) {
+                setState(() {
+                  user.country = value!;
+                });
+              },
+              decoration: const InputDecoration(hintText: 'Country'),
+            ),
+            const SizedBox(
+              height: 25,
+            ),
+            DateTimeField(
+              format: DateFormat("yyyy-MM-dd"),
+              onShowPicker: (context, currentValue) {
+                return showDatePicker(
+                    context: context,
+                    firstDate: DateTime(1900),
+                    initialDate: DateTime(2000),
+                    lastDate: DateTime.now()
+                );
+              },
+              validator: (DateTime? value) {
+                if (value == null) {
+                  return 'Please enter some text';
+                }
+                return null;
+              },
+              onSaved: (DateTime? value) {
+                setState(() {
+                  user.birthdate = DateFormat('dd/MM/yyyy').format(value!);
+                });
+              },
+              decoration: const InputDecoration(hintText: 'Birthdate'),
             ),
             const SizedBox(
               height: 25,
---
+++ lib/models/user_model.dart
@@ -0,0 +1,46 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class UserModel {
+  UserModel({
+    required this.firstname,
+    required this.lastname,
+    required this.country,
+    required this.city,
+    required this.birthdate,
+    required this.image
+  });
+
+  String firstname;
+  String lastname;
+  String country;
+  String city;
+  String birthdate;
+  String image;
+
+  factory UserModel.fromMap(Map<String, dynamic> json) => UserModel(
+    firstname: json["firstname"],
+    lastname: json["lastname"],
+    country: json["country"],
+    city: json["city"],
+    birthdate: json["birthdate"],
+    image: json["image"],
+  );
+
+  factory UserModel.fromSnapshot(DocumentSnapshot<Object?>? object) => UserModel(
+    firstname: object!["firstname"],
+    lastname: object!["lastname"],
+    country: object!["country"],
+    city: object!["city"],
+    birthdate: object!["birthdate"],
+    image: object!["image"],
+  );
+
+  Map<String, dynamic> toMap() => {
+    "firstname": firstname,
+    "lastname": lastname,
+    "country": country,
+    "city": city,
+    "birthdate": birthdate,
+    "image": image
+  };
+}
